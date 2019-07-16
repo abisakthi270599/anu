5ce2b74aa313c815c1d6e09f3cde71a75967f533
abisakthi2759 <52281389+abisakthi2759@users.noreply.github.com>
2019-07-16 03:04:51 -0700

    Rename vowel.py to vo.py

---
+++ vo.py
@@ -0,0 +1,7 @@
+ch=input()
+if(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+	print("Vowel")
+elif(ch>='A' and ch<='Z' or ch>='a' and ch<='z'):
+	print("Consonent")
+else:
+	print("invalid")
