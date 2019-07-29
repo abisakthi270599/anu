883e3ba5c0da3851be0dbfc4bced5300fc01b35a
abisakthi2759 <52281389+abisakthi2759@users.noreply.github.com>
2019-07-29 14:57:53 +0530

    Create 52.py

---
+++ 52.py
@@ -0,0 +1,22 @@
+i=int(input())
+if(i==1):
+	print("One")
+elif(i==2):
+	print("Two")
+elif(i==3):
+	print("Three")
+elif(i==4):
+	print("Four")
+elif(i==5):
+	print("Five")
+elif(i==6):
+	print("Six")
+elif(i==7):
+	print("Seven")
+elif(i==8):
+	print("Eight")
+elif(i==9):
+	print("Nine")
+else:
+	print("Ten")
+	
